104d81ef24215ad133071f9b27590d3d3a42716f
Arjon Kapaj <98480212+arjonkapaj@users.noreply.github.com>
2022-02-11 20:11:07 +0100

    Update movement.py

--- movement.py
+++ movement.py
@@ -315,3 +315,23 @@ df_compare = pd.DataFrame({'Model': df_1['Model'],
 df_compare.plot(x="Model", y=['200_records_train', '100_records_train'], kind="bar")
 
 df_compare.plot(x="Model", y=['200_records_test', '100_records_test'], kind="bar")
+
+# finding the most important feautures
+import pandas as pd
+import numpy as np
+data = df
+X = data.iloc[:,0:20]  #independent columns
+y = data.iloc[:,-1]    #target column 
+from sklearn.ensemble import ExtraTreesClassifier
+import matplotlib.pyplot as plt
+model = ExtraTreesClassifier()
+model.fit(X,y)
+print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
+#plot graph of feature importances for better visualization
+feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+feat_importances.nlargest(10).plot(kind='barh')
+plt.show()
+
+#best features
+best_features = list(subsets_wraper.iloc[7]['feature_names'])
+best_features
